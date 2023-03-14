a8e947843193b23648dd1c2954918a5f58c24e3d
Anton Src <anton.source@gmail.com>
2023-03-14 20:14:51 +0700

    js a href onclick add

--- src/log.js
+++ src/log.js
@@ -1,10 +1,27 @@
-var fTD = document.getElementById("firstTD");
-fTD.style.background = "red";
-console.log(fTD.clientWidth);
 
-var newWidth = fTD.clientWidth + 200;
-fTD.style.width = newWidth + "px";
-console.log(newWidth);
 
-var graytd = document.getElementById("grayTD");
-graytd.innerHTML = "Новый текст";
+function changeDisplay() {
+    var dostoevsky = document.getElementById("dostoevsky");
+    if(dostoevsky.style.display == "none") {
+        dostoevsky.style.display = "block";
+    }
+    else{
+        dostoevsky.style.display = "none";
+    }
+}
+
+window.onload = changeFirstTD;
+function changeFirstTD() {
+    var fTD = document.getElementById("firstTD");
+    if (fTD.style.background == "aqua"){
+        fTD.style.background = "red";
+        console.log(fTD.clientWidth);
+        fTD.style.width = (fTD.clientWidth + 200) + "px";
+    }
+    else{
+        fTD.style.background = "aqua";
+        fTD.style.width = (fTD.clientWidth - 200) + "px";
+    }
+
+}
+
